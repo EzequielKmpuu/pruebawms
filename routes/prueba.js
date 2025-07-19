09780a8a9493b33d266bfcb3e16fbcaf97c6b0d0
EzequielKmpuu <eomarsala@gmail.com>
2025-07-19 12:22:07 +0200

    Delete routes directory

--- routes/prueba.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Mostrar la página de pruebas
-router.get('/prueba', (req, res) => {
-  res.render('prueba');
-});
-
-module.exports = router;
